b6b20bc49a07712153af0f5ece6b82197b6f4768
budds300 <budds300@gmail.com>
2021-06-19 02:30:10 +0300

    addition of prodconfig,test config and devconfig class in config file

--- config.py
+++ config.py
@@ -12,4 +12,29 @@ class Config:
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     
+    class ProdConfig(Config):
+     '''
+    Production configuration child class
     
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = False
+
+class TestConfig(Config):
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'Test':TestConfig
+}
