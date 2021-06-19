8baeba21a2e4d049016580e6ad5e6690b5e133fa
budds300 <budds300@gmail.com>
2021-06-20 00:09:56 +0300

    initiaization of mail import

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,26 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads,IMAGES
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
-
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-login_manager=LoginManager()
-login_manager.session.protection='strong'
+photos=UploadSet('photos',IMAGES)
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-simple=SimpleMDE()
+simple = SimpleMDE()
 
 def create_app(config_name):
-    app=Flask(__name__)
-    # creating app configuration
+    app =Flask(__name__)
+
+    # creating app configurations
     app.config.from_object(config_options[config_name])
-    
-    # initializing the flask extention
+
+    #Initializing the flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
@@ -32,9 +34,10 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix='/auth')
-    
-    
+    #will add the views and forms
+    # setting config 
+    # configure UploadSet
     configure_uploads(app,photos)
-    from ..requests import configure_request
+    from .request import configure_request
     configure_request(app)
     return app
