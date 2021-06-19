efad401640be829ce0433834ecd3b2e0107cb92a
budds300 <budds300@gmail.com>
2021-06-19 14:04:00 +0300

    addition of class BlogForm, Commentsform and update proile

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
+
+
+class BlogForm(FlaskForm):
+    title=StringField('Blog Title',validators=[Required()])
+    content= TextAreaField('Make a blog', validators=[Required()])
+    SubmitField=SubmitField('submit blog')
+    
+    
+class CommentsForm(FlaskForm):
+    comments= TextAreaField(validators=[Required()])
+    submit = SubmitField('submit blog')
+    
+class UpdateProfile(FlaskForm):
+    bio= TextAreaField('Create your bio',validators=[Required()])
+    submit=SubmitField('submit')
