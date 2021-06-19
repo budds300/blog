8b76b3eb9eec08e65d7ca6e48c306ebf1e730295
budds300 <budds300@gmail.com>
2021-06-20 00:30:12 +0300

    corrected typos

--- app/request.py
+++ app/request.py
@@ -1,18 +1,21 @@
 import requests
 from .models import Quote
 
-#Getting the Quote base url
+
+
+# Getting the Quote base url
 base_url = None
+
 
 def configure_request(app):
     global base_url
-    base_url= app.config['QUOTES_API_URL']
-    
+    base_url = app.config['QUOTES_API_URL']
+
 def get_quotes():
-    ''' 
-    function gets the json response to our url
     '''
-    get_quotes_response = requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
-    print(get_quotes_response)
+    Function that gest the json response to our url
+    '''
+    get_quote_response = requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
+    print(get_quote_response)
     
-    return get_quotes_response
+    return get_quote_response
