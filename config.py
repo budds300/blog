96233836fc852f314539d13b5aa1b1a8f0be8299
budds300 <budds300@gmail.com>
2021-06-20 00:28:21 +0300

    addition of mail configurations in the config file

--- config.py
+++ config.py
@@ -1,26 +1,34 @@
 import os
-
 class Config:
     '''
-    General configurations parent
+    General configuration parent class
     '''
     QUOTES_API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY= os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST= 'app/static/photos'
-    
-     #simple mde configurations
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:hello@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    #simple mde configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    
+
+
 class ProdConfig(Config):
-     '''
+    '''
     Production configuration child class
     
     Args:
         Config:The parent configuration class with General configuration settings
     '''
-     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = False
 
 class TestConfig(Config):
     pass
