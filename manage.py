bdf200ace9321d6c2f86c1fbf980d1a443b9a077
budds300 <budds300@gmail.com>
2021-06-19 03:02:18 +0300

    updated manage file

--- manage.py
+++ manage.py
@@ -1,1 +1,25 @@
 from app import create_app,db
+from flask_script import Manager,Server
+from .app.models import User, Role,Comment,Subscribe
+from flask_migrate import Migrate,MigrateCommand
+
+# create app instance
+app = create_app('production')
+app = create_app('development')
+migrate = Migrate(app,db)
+manager.add_command('server',Server)
+manage.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    '''Run unit tests'''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(test)
+    
+    
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Role = Role,Comment = Comment,Subscribe=Subscribe)
+if  __name__ == "__main__":
+    manager.run()
