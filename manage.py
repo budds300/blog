7edb87183ec6099b1f8b6f20f86e3cb61ec9e179
budds300 <budds300@gmail.com>
2021-06-22 16:12:59 +0300

    corrected files in requirements file

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate, MigrateCommand
 
 # app = create_app("test")
 
-# app=create_app('development')
-app=create_app('production')
+app=create_app('development')
+# app=create_app('production')
 
 manager = Manager(app)
 
